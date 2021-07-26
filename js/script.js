c03bdd08fd303e57032414ab10c06baccef8a707
Michele-Carbone <michele.carboneetlab@gmail.com>
2021-07-26 23:04:17 +0800

    array-icons

---
+++ js/script.js
@@ -0,0 +1,107 @@
+const icons = [
+    {
+        name: 'cat',
+        preferix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+
+    {
+        name: 'crow',
+        preferix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+
+    {
+        name: 'dog',
+        preferix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+
+    {
+        name: 'dove',
+        preferix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+
+    {
+        name: 'dragon',
+        preferix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+
+    {
+        name: 'horse',
+        preferix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+
+    {
+        name: 'hippo',
+        preferix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+
+    {
+        name: 'fish',
+        preferix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'carrot',
+        preferix: 'fa-',
+        type: 'vegetable',
+        family: 'fas',
+    },
+    {
+        name: 'apple-alt',
+        preferix: 'fa-',
+        type: 'vegetable',
+        family: 'fas',
+    },
+    {
+        name: 'lemon',
+        preferix: 'fa-',
+        type: 'vegetable',
+        family: 'fas',
+    },
+    {
+        name: 'pepper-hot',
+        preferix: 'fa-',
+        type: 'vegetable',
+        family: 'fas',
+    },
+    {
+        name: 'user-astronaut',
+        preferix: 'fa-',
+        type: 'user',
+        family: 'fas',
+    },
+    {
+        name: 'user-graduate',
+        preferix: 'fa-',
+        type: 'user',
+        family: 'fas',
+    },
+    {
+        name: 'user-ninja',
+        preferix: 'fa-',
+        type: 'user',
+        family: 'fas',
+    },
+    {
+        name: 'user-secret',
+        preferix: 'fa-',
+        type: 'user',
+        family: 'fas',
+    },
+];
+
+
